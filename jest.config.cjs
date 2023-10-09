8f35b46c244720081c877807910f1f7b0478a40e
Masahiro Wada <argon.argon.argon@gmail.com>
2023-10-09 14:55:06 +0000

    start migration to ts

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,19 @@
 /** @type {import("ts-jest/dist/types").InitialOptionsTsJest} */
 module.exports = {
+  preset: 'ts-jest/presets/default-esm',
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['./test/setup.js'],
+  setupFilesAfterEnv: ['./test/setup.ts'],
 };
